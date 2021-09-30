29ab5fc20e8085c814b9457aa1a19c215bb8b650
vkengine <v.kmr@yahoo.com>
2021-10-01 00:26:24 +0530

    [30-09-2021] [medium] [strings] longest palindrome

--- strings/medium/longest_palindrome.py
+++ strings/medium/longest_palindrome.py
@@ -1,17 +1,34 @@
+"""
+Write a function that, given a string, returns its longest palindromic substring.
+A palindrome is defined as a string that's written the same forward and backward.
+Note that single-character strings are palindromes.
+You can assume that there will only be one longest palindromic substring.
+
+Sample Input
+string = "abaxyzzyxf"
+Sample Output
+"xyzzyx"
+"""
+
+
+# O(n^2) time | O(n) space
 def longestPalindromicSubstring(string):
-    currentLongest = [0, 1]
+    # Write your code here.
+    current_longest = [0, 1]
     for i in range(1, len(string)):
-        odd = getLongestPalindromeFrom(string, i - 1, i + 1)
-        even = getLongestPalindromeFrom(string, i - 1, i)
+        odd = get_longest_palindrome(string, i - 1, i + 1)
+        even = get_longest_palindrome(string, i - 1, i)
         longest = max(odd, even, key=lambda x: x[1] - x[0])
-        currentLongest = max(longest, currentLongest, key=lambda x: x[1] - x[0])
-    return string[currentLongest[0]: currentLongest[1]]
+        current_longest = max(longest, current_longest, key=lambda x: x[1] - x[0])
+
+    return string[current_longest[0]: current_longest[1]]
 
 
-def getLongestPalindromeFrom(string, leftIdx, rightIdx):
-    while leftIdx >= 0 and rightIdx < len(string):
-        if string[leftIdx] != string[rightIdx]:
+def get_longest_palindrome(string, left_idx, right_idx):
+    while left_idx >= 0 and right_idx < len(string):
+        if string[left_idx] != string[right_idx]:
             break
-        leftIdx -= 1
-        rightIdx += 1
-    return [leftIdx + 1, rightIdx]
+        left_idx -= 1
+        right_idx += 1
+
+    return [left_idx + 1, right_idx]
