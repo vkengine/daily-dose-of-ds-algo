722bb5e85b4e614b5507aa528dc53f211e4740d1
vkengine <v.kmr@yahoo.com>
2021-10-05 23:53:21 +0530

    [05-10-2021] [medium] [strings] longest_substring_without_duplication

---
+++ strings/medium/longest_substring_without_duplication.py
@@ -0,0 +1,26 @@
+"""
+Longest Substring Without Duplication
+Write a function that takes in a string and returns its longest substring without duplicate characters.
+
+You can assume that there will only be one longest substring without duplication.
+
+Sample Input
+string = "mentisacap"
+Sample Output
+"mentisac"
+"""
+
+
+# O(n) time | O(min(n, a)) space - where n is the length of the input string and a
+# is the length of the character alphabet represented in the input string
+def longestSubstringWithoutDuplication(string):
+    lastSeen = {}
+    longest = [0, 1]
+    startIdx = 0
+    for i, char in enumerate(string):
+        if char in lastSeen:
+            startIdx = max(startIdx, lastSeen[char] + 1)
+        if longest[1] - longest[0] < i + 1 - startIdx:
+            longest = [startIdx, i + 1]
+        lastSeen[char] = i
+    return string[longest[0]: longest[1]]
